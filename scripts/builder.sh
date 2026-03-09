01f81c2000fbe337d2ea5dc510e46bc46af3b98c
ediaz23 <esteban.diazt23@gmail.com>
2026-03-08 22:23:01 -0300

    [IMP] runs

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -2,7 +2,7 @@
 set -e
 
 # docker run --rm -it -v "$PWD:/work" xplayer-builder "$@"
-docker run --rm -it -v "$PWD:/work" xplayer-builder ./autogen.sh --enable-run-in-source-tree
+docker run --rm -it -v "$PWD:/work" xplayer-builder ./autogen.sh
 docker run --rm -it -v "$PWD:/work" xplayer-builder make CFLAGS="-Wno-error -Wno-deprecated-declarations \
     -Wno-incompatible-pointer-types"
 docker run --rm -it -v "$PWD:/work" xplayer-builder glib-compile-schemas /work/data
--- scripts/run.sh
+++ scripts/run.sh
@@ -23,8 +23,14 @@ xhost +local:docker
 # Ejecutar el contenedor Docker
 docker run --name "$CONTAINER_NAME" -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /run/user/1000/pulse:/run/user/1000/pulse \
   -v ".:/work" \
+  -v "$HOME:/home" \
+  -w /work/src \
   -e GSETTINGS_SCHEMA_DIR=/work/data \
-  --privileged --shm-size=3g -it xplayer-builder ./src/xplayer
+  -e G_MESSAGES_DEBUG=all \
+  -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
+  -e XDG_RUNTIME_DIR=/run/user/1000 \
+  --privileged --shm-size=3g -it xplayer-builder ./xplayer
 
 cleanup
