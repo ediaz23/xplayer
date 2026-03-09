b804eccfa7d02d8d052df14d9f57104ed4bac6df
ediaz23 <esteban.diazt23@gmail.com>
2026-03-08 21:31:01 -0300

    [IMP] run script

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-docker run --rm -it -v "$PWD:/work" xplayer-builder "$@"
+# docker run --rm -it -v "$PWD:/work" xplayer-builder "$@"
+docker run --rm -it -v "$PWD:/work" xplayer-builder ./autogen.sh --enable-run-in-source-tree
+docker run --rm -it -v "$PWD:/work" xplayer-builder make CFLAGS="-Wno-error -Wno-deprecated-declarations \
+    -Wno-incompatible-pointer-types"
+docker run --rm -it -v "$PWD:/work" xplayer-builder glib-compile-schemas /work/data
